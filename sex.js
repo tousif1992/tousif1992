0f825b18dbe41739556601d757ae9659dc261432
tousif ahmed <130003645+tousif1992@users.noreply.github.com>
2023-05-17 00:43:41 +0530

    sex.js

--- sex.js
+++ sex.js
@@ -1,57 +1,130 @@
-// Get DOM elements
-const product = document.querySelector('.product');
-const addToCartButton = product.querySelector('.add-to-cart');
-const cart = document.querySelector('.cart');
-const cartItems = cart.querySelector('.cart-items');
-const total = cart.querySelector('.total');
-const checkoutButton = cart.querySelector('.checkout');
+// cart
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
+// open cart
+cartIcon.onclick = () => {
+	cart.classList.add("active");
+};
 
-// Initialize cart
-let cartTotal = 0;
-const cartData = {};
+closeCart.onclick = () => {
+	cart.classList.remove("active");
+};
+// Cart working JS
+if (document.readyState == "loading"){
+	document.addEventListener("DOMContentLoaded", ready);
+} else {
+	ready();
+};
+// Making function
+function ready() {
+	// Remove items from cart
+	var removeCartButtons = document.getElementsByClassName("cart-remove");
+	console.log(removeCartButtons);
+	for (var i = 0; i < removeCartButtons.length; i++) {
+		var button = removeCartButtons[i];
+		button.addEventListener("click", removeCartItem);
+	} 
+// quantity changes
+var quantityInputs = document.getElementsByClassName("cart-quantity");
+for (var i = 0; i < quantityInputs.length; i++) {
+	var input = quantityInputs[i];
+	input.addEventListener("change", quantityChanged);
+}
+// Add To Cart
+var addCart = document.getElementsByClassName("add-Cart");
+for (var i = 0; i < addCart.length; i++) {
+	var button = addCart[i];
+	button.addEventListener("click", addCartClicked);
+}
+ // Buy Button Work
+document
+.getElementsByClassName("btn-buy")[0]
+.addEventListener("click", buyButtonClicked);
+}
+ // Buy Botton
+function buyButtonClicked() {
+	alert("Your Order Is Placed");
+	var cartContent = document.getElementsByClassName("cart-content")[0];
+	while (cartContent.hasChildNodes()){
+		cartContent.removeChild(cartContent.firstChild);
+	}
+	updatetotal();
+}
 
-// Add event listener for add-to-cart button
-addToCartButton.addEventListener('click', () => {
-  const product-Name = product.querySelector('h2').innerText;
-  const productPrice = parseFloat(product.querySelector('.price').innerText.replace('$', ''));
 
-  // Add item to cart data
-  if (cartData[product-Name]) {
-    cartData[product-Name].quantity += 1;
-  } else {
-    cartData[product-Name] = {
-      price: productPrice,
-      quantity: 1
-    };
-  }
+// Remove Items From Cart
+function removeCartItem(event) {
+	var buttonClicked = event.target;
+	buttonClicked.parentElement.remove();
+	updatetotal();
+}
+// Quantity changes
+function quantityChanged(event) {
+	var input = event.target;
+	if (isNaN(input.value) || input.value <= 0) {
+	input.value = 1;
+	}
+	updatetotal();
+}
+// Add To cart
+ function addCartClicked(event) {
+	var button  = event.target;
+	var shopProducts = button.parentElement;
+	var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+	var price = shopProducts.getElementsByClassName("price")[0].innerText;
+	var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+	addProductToCart(title, price, productImg);
+	updatetotal();
+}
+function addProductToCart(title, price, productImg) {
+	var cartShopBox = document.createElement("div");
+	cartShopBox.classList.add("cart-box");
+	var cartItems = document.getElementsByClassName("cart-content")[0];
+	var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+	for (var i = 0; i < cartItemsNames.length; i++) {
+		if (cartItemsNames[i].innerText  == title) {
+		alert("You have already add this item to cart");
+		return;
+		}
+	}
+	
 
-  // Update cart UI
-  updateCart();
-});
+var cartBoxContent = `
+                           <img src="${productImg}" alt="" class="cart-img">
+                            <div class="detail-box">
+                                <div class="cart-product-title">${title}</div>
+                                <div class="cart-price">${price}</div>
+                                <input type="number" value="1" class="cart-quantity">
+                            </div>
+                            <i class="fas fa-trash-alt cart-remove" ></i>`;
+cartShopBox.innerHTML = cartBoxContent;
+cartItems.append(cartShopBox);
+cartShopBox
+.getElementsByClassName("cart-remove")[0]
+.addEventListener("click", removeCartItem);
+cartShopBox
+.getElementsByClassName("cart-quantity")[0]
+.addEventListener("change", quantityChanged);
 
-// Add event listener for checkout button
-checkoutButton.addEventListener('click', () => {
-  // Send cart data to server for processing
-  console.log(cartData);
+}
+// Update Total
+function updatetotal() {
+	var cartContent = document.getElementsByClassName("cart-content")[0];
+	var cartBoxes = cartContent.getElementsByClassName("cart-box");
+	var total = 0;
+	for (var i = 0; i < cartBoxes.length; i++) {
+		var cartBox = cartBoxes[i];
+		var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+		var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+		var price = parseFloat(priceElement.innerText.replace("₹", ""));
+		var quantity = quantityElement.value;
+		total = total + (price * quantity);
+	}
+		// If Price content Some cents Value
+		total = Math.round(total * 100) / 100;
 
-  // Reset cart
-  cartTotal = 0;
-  cartData = {};
-  cartItems.innerHTML = '';
-  total.innerText = `Total: $${cartTotal.toFixed(2)}`;
-});
+		document.getElementsByClassName("total-price")[0].innerText = "₹" + total;
+	
+}
 
-// Update cart UI function
-function updateCart() 
-  cartItems.innerHTML = '';
-  cartTotal = 0;
-
-  for (const [product-Name, productData] of Object.entries(cartData)) {
-    const li = document.createElement('li');
-    li.innerText = `${productName} x ${productData.quantity} - $${(productData.price * productData.quantity).toFixed(2)}`;
-    cartItems.appendChild(li);
-
-    cartTotal += productData.price * productData.quantity;
-  }
-
-  total.innerText = `Total: $${cartTotal.toFixed(2)}`;
